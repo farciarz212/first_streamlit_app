d6f47502d916eee4548c7e2adf9093f608189a73
farciarz <116302569+farciarz212@users.noreply.github.com>
2022-10-20 13:49:48 -0400

    changed header location

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
